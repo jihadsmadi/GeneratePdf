37a311dd99d2b9bb81e2411117b2d1d494d59cae
jihadsmadi <80224241+jihadsmadi@users.noreply.github.com>
2024-10-22 01:21:04 +0300

    UPDATE Generate File

--- routes/pdfRoutes.js
+++ routes/pdfRoutes.js
@@ -1,33 +1,69 @@
 import express from "express";
+import fs from "fs";
 import { generatePDF } from "../services/pdfService.js";
 import { fetchData } from "../services/httpFetch.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const router = express.Router();
 const url = "https://6714fd1b33bc2bfe40b8fec4.mockapi.io/api/v1/student";
 let data;
+
 fetchData(url)
-  .then((data) => {
-    if (data && data.length > 0) {
-      return data[0]["student"];
+  .then((responseData) => {
+    if (responseData && responseData.length > 0) {
+      data = responseData[0]["student"];
     } else {
       throw new Error("No data found");
     }
   })
-  .then((student) => (data = student))
   .catch((error) => {
-    console.error("Error:", error.message);
+    console.error("Error fetching data:", error.message);
   });
 
 router.get("/generate-pdf", async (req, res) => {
   try {
-    const pdfBuffer = await generatePDF(data); // Call the function directly
-    res.setHeader(
-      "Content-disposition",
-      'attachment; filename="studentInfo.pdf"'
-    );
-    res.setHeader("Content-type", "application/pdf");
-    res.send(pdfBuffer);
+    if (!data) {
+      throw new Error("Data not loaded");
+    }
+
+    const pdfBuffer = await generatePDF(data);
+
+    // Define the file path
+    const filePath = path.join(__dirname, "studentInfo.pdf");
+
+    // Write the buffer to a file
+    fs.writeFile(filePath, pdfBuffer, (err) => {
+      if (err) {
+        console.error("Error writing PDF file:", err);
+        res.status(500).send("Error generating PDF");
+        return;
+      }
+
+      res.setHeader(
+        "Content-disposition",
+        'attachment; filename="studentInfo.pdf"'
+      );
+      res.setHeader("Content-type", "application/pdf");
+      res.sendFile(filePath, (err) => {
+        if (err) {
+          console.error("Error sending PDF file:", err);
+          res.status(500).send("Error sending PDF");
+        } else {
+          // Delete the file after sending
+          fs.unlink(filePath, (err) => {
+            if (err) {
+              console.error("Error deleting PDF file:", err);
+            }
+          });
+        }
+      });
+    });
   } catch (error) {
+    console.error("Error generating PDF:", error.message);
     res.status(500).send("Error generating PDF");
   }
 });
--- services/pdfService.js
+++ services/pdfService.js
@@ -1,106 +1,354 @@
-import PDFDocument from "pdfkit";
+import puppeteer from "puppeteer-core";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-export function generatePDF(studentData) {
-  return new Promise((resolve, reject) => {
-    const doc = new PDFDocument({
-      margin: { top: 10, left: 50, right: 30, bottom: 30 },
-    });
-    let buffers = [];
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// Function to replace placeholders in the HTML template
+function replacePlaceholders(template, data) {
+  return template.replace(/{{(\w+)}}/g, (match, key) => data[key] || "");
+}
 
-    doc.on("data", buffers.push.bind(buffers));
-    doc.on("end", () => resolve(Buffer.concat(buffers)));
+export async function generatePDF(student) {
+  const htmlContent = `
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <style>
+        body {
+            font-family: "Roboto", sans-serif;
+            margin: 0;
+            padding: 10px;
+            background-color: #f8f8f8;
+        }
 
-    let imagePath =
-      "D:/Work Space/Programming/Back End/Node Js/project/images/output-onlinepngtools.png";
+        header {
+            text-align: center;
+            margin-bottom: 10px;
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+            align-items: center;
+        }
 
-    const pageWidth = doc.page.width;
-    const imageWidth = 200; // Reduced image size
-    const imageX = (pageWidth - imageWidth) / 2;
+        header .title {
+            max-width: 200px;
+        }
 
-    doc.image(imagePath, imageX, 10, {
-      width: imageWidth,
-    });
-    doc.moveDown(1); // Reduced space after the image
+        header img {
+            max-width: 100px;
+        }
 
-    doc
-      .fontSize(18)
-      .fillColor("#1e5edf")
-      .text("Student Details", { align: "center" });
-    doc.moveDown(0.5);
-    doc.fontSize(10).fillColor("black").text(`ID: ${studentData["id"]}`);
-    doc.text(
-      `Name: ${studentData["firstName"] + " " + studentData["lastName"]}`
-    );
-    doc.text(`Age: ${studentData["age"]}`);
-    doc.text(`Grade: ${studentData["grade"]}`);
-    doc.text(`Phone Number: ${studentData["phoneNumber"]}`);
-    doc.text(`Email:${studentData["email"]}`);
-    let address = studentData["address"];
-    doc.text(
-      `Address: ${address["street"]},${address["city"]}, ${address["state"]},${address["postalCode"]}`
-    );
-    doc.moveDown(0.5);
+        .container {
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            gap: 10px;
+            margin: 0 auto;
+            max-width: 1000px;
+        }
 
-    doc.fontSize(14).fillColor("#ff6e00").text("Subjects", { underline: true });
-    doc.moveDown(0.5);
-    const subjects = studentData["subjects"];
+        .personal-info,
+        .parents-info,
+        .emergencyContact,
+        .subjects,
+        .results,
+        .enroll-status {
+            background-color: #fff;
+            box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
+            border-radius: 10px;
+            padding: 10px;
+            margin: 2px 0;
+        }
 
-    subjects.forEach((subject) => {
-      doc.fontSize(10).fillColor("black").text(`Subject: ${subject.name}`);
-      doc.text(`Teacher: ${subject.teacher}`);
-      doc.text(`Marks: ${subject.marks}`);
-      doc.moveDown(0.5);
-    });
 
-    doc.fontSize(14).fillColor("#1e5edf").text("Results", { underline: true });
-    doc.moveDown(0.5);
-    doc
-      .fontSize(10)
-      .fillColor("black")
-      .text(`Total Marks: ${studentData["results"].totalMarks}`);
-    doc.text(`Percentage: ${studentData["results"].percentage}%`);
-    doc.text(`Grade: ${studentData["results"].grade}`);
-    doc.moveDown(0.5);
+        .personal-info {
+            grid-column: 1 / 2;
+            grid-row: 1 / 2;
+        }
 
-    doc
-      .fontSize(14)
-      .fillColor("#ff6e00")
-      .text("Parent Details", { underline: true });
-    doc.moveDown(0.5);
-    doc
-      .fontSize(10)
-      .fillColor("black")
-      .text(
-        `Father: ${studentData["parentDetails"]["father"].name}, Occupation: ${studentData["parentDetails"]["father"].occupation}, Phone: ${studentData["parentDetails"]["father"].phoneNumber}`
-      );
-    doc.text(
-      `Mohter: ${studentData["parentDetails"]["mother"].name}, Occupation: ${studentData["parentDetails"]["mother"].occupation}, Phone: ${studentData["parentDetails"]["mother"].phoneNumber}`
-    );
-    doc.moveDown(0.5);
+        .personal-info div {
+            display: flex;
+            align-items: center;
+            gap: 10px;
+            border: 1px solid #eee;
+            background-color: #fafafa;
+            margin-bottom: 5px;
+            padding: 5px;
+            border-radius: 5px;
 
-    doc
-      .fontSize(14)
-      .fillColor("#1e5edf")
-      .text("Emergency Contact", { underline: true });
-    doc.moveDown(0.5);
-    doc
-      .fontSize(10)
-      .fillColor("black")
-      .text(
-        `Name: ${studentData["emergencyContact"].name}, Relationship: ${studentData["emergencyContact"].relationship}, Phone: ${studentData["emergencyContact"].phoneNumber}`
-      );
-    doc.moveDown(0.5);
+        }
 
-    doc
-      .fontSize(14)
-      .fillColor("#ff6e00")
-      .text("Enrollment Status", { underline: true });
-    doc.moveDown(0.5);
-    doc
-      .fontSize(10)
-      .fillColor("black")
-      .text(`Status: ${studentData["enrollmentStatus"]}`);
+        .parents-info {
+            grid-column: 2 / 3;
+            grid-row: 1 / 2;
+        }
 
-    doc.end();
+        .parents-info .content {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            gap: 10px;
+        }
+
+        .parents-info .content>div {
+            border: 1px solid #eee;
+            background-color: #fafafa;
+            height: 150px;
+            display: flex;
+            justify-content: space-evenly;
+            flex-direction: column;
+            padding: 10px;
+            width: 100%;
+        }
+
+
+        .emergencyContact {
+            grid-column: 1 / 3;
+            grid-row: 2 / 3;
+        }
+
+        .emergencyContact .contant {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            gap: 10px;
+        }
+
+        .emergencyContact .contant div {
+            border: 1px solid #eee;
+            background-color: #fafafa;
+            padding: 10px 40px;
+            border-radius: 5px;
+
+        }
+
+        .emergencyContact .contant .name {
+            flex: 1;
+        }
+
+        .subjects {
+            grid-column: 1 / 2;
+            grid-row: 3 / 4;
+        }
+
+        .results {
+            grid-column: 2 / 3;
+            grid-row: 3 / 4;
+
+        }
+
+        .results .content {
+            border: 1px solid #eee;
+            background-color: #fafafa;
+            border-radius: 5px;
+            padding: 20px;
+            width: 225px;
+            height: 150px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+        }
+
+        .enroll-status {
+            grid-column: 1 / 3;
+            grid-row: 4 / 5;
+        }
+
+        h1 {
+            color: #1e5edf;
+        }
+
+        h2 {
+            font-size: 18px;
+            font-weight: 500;
+            color: #444;
+            margin-bottom: 5px;
+        }
+
+        span {
+            font-size: 14px;
+            color: #666;
+        }
+
+        .card {
+            border: 1px solid #eee;
+            padding: 10px;
+            margin-bottom: 10px;
+            border-radius: 5px;
+            background-color: #fafafa;
+        }
+
+        .card .name,
+        .card .techerName,
+        .card .marks {
+            margin: 3px 0;
+        }
+
+        .results  {
+            text-align: center;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+
+        }
+
+        .results .total,
+        .results .percentage,
+        .results .gradeSympol {
+            font-size: 14px;
+            color: #ff6e00;
+            margin: 5px 0;
+        }
+
+        .enroll-status {
+            font-weight: bold;
+            text-align: center;
+            padding: 10px;
+            background-color: #cbf0e8;
+            color: #00796b;
+            border-radius: 5px;
+            margin: 5px 0;
+        }
+          .enrollmentStatus{
+           color: #1e5edf;
+          }
+    </style>
+</head>
+
+<body>
+    <header>
+        <img src="https://apostrophe.com.tr/wp-content/uploads/2021/02/apostrophe-Logo-short-form.png" alt="">
+        <img src="https://apostrophe.com.tr/wp-content/uploads/2020/08/output-onlinepngtools.png" alt="" class="title">
+    </header>
+    <div class="container">
+    <div class="personal-info">
+      <h1>Personal Information</h1>
+      <div class="id">
+        <span>Student ID:</span>
+        <span id="student-id">${student.id}</span>
+      </div>
+      <div class="name">
+        <span>Full Name:</span>
+        <span id="student-name">${student.firstName} ${student.lastName}</span>
+      </div>
+      <div class="age">
+        <span>Age:</span>
+        <span id="student-age">${student.age}</span>
+      </div>
+      <div class="grade">
+        <span>Grade:</span>
+        <span id="student-grade">${student.grade}</span>
+      </div>
+      <div class="phone">
+        <span>Phone Number:</span>
+        <span id="student-phone">${student.phoneNumber}</span>
+      </div>
+      <div class="address">
+        <span>Address:</span>
+        <span id="student-address">${student.address.street}, ${
+    student.address.city
+  }, ${student.address.state}, ${student.address.postalCode}</span>
+      </div>
+    </div>
+    <div class="parents-info">
+      <h1>Parents Information</h1>
+      <div class="content">
+        <div class="father">
+          <div class="name"><span>Father Name:</span> <span id="father-name">${
+            student.parentDetails.father.name
+          }</span></div>
+          <div class="occupation"><span>Occupation:</span> <span id="father-occupation">${
+            student.parentDetails.father.occupation
+          }</span></div>
+          <div class="phone"><span>Phone Number:</span> <span id="father-phone">${
+            student.parentDetails.father.phoneNumber
+          }</span></div>
+        </div>
+        <div class="mother">
+          <div class="name"><span>Mother Name:</span> <span id="mother-name">${
+            student.parentDetails.mother.name
+          }</span></div>
+          <div class="occupation"><span>Occupation:</span> <span id="mother-occupation">${
+            student.parentDetails.mother.occupation
+          }</span></div>
+          <div class="phone"><span>Phone Number:</span> <span id="mother-phone">${
+            student.parentDetails.mother.phoneNumber
+          }</span></div>
+        </div>
+      </div>
+    </div>
+    <div class="emergencyContact">
+      <h1>Emergency Contact Information</h1>
+      <div class="contant">
+        <div class="name"><span>Name:</span> <span id="emergency-name">${
+          student.emergencyContact.name
+        }</span></div>
+        <div class="relationship"><span>Relationship:</span> <span id="emergency-relationship">${
+          student.emergencyContact.relationship
+        }</span></div>
+        <div class="phone"><span>Phone Number:</span> <span id="emergency-phone">${
+          student.emergencyContact.phoneNumber
+        }</span></div>
+      </div>
+    </div>
+    <div class="subjects">
+      <h1>Subjects</h1>
+        ${student.subjects
+          .map(
+            (subject) => `
+          <div class="card">
+            <div class="subject-name"><span>Subject Name:</span> <span>${subject.name}</span></div>
+            <div class="subject-teacher"><span>Teacher:</span> <span>${subject.teacher}</span></div>
+            <div class="subject-marks"><span>Marks:</span> <span>${subject.marks}</span></div>
+          </div>
+        `
+          )
+          .join("")}
+    </div>
+    <div class="results">
+  <h1>Results</h1>
+  <div class="content">
+    <div><span>Total Marks:</span> <span class="total">${
+      student.results.totalMarks
+    }</span></div>
+    <div><span>Percentage:</span> <span class="percentage">${
+      student.results.percentage
+    }</span></div>
+    <div><span>Grade:</span> <span class="gradeSympol">${
+      student.results.grade
+    }</span></div>
+  </div>
+</div>
+    <div class="enroll-status">Enrollment Status: <span class="enrollmentStatus">${
+      student.enrollmentStatus
+    }</span></div>
+  </div>
+
+    <script src="services/pdfService.js"></script>
+</body>
+</html> `;
+
+  // Launch Puppeteer
+  const browser = await puppeteer.launch({
+    executablePath: "C:\\Program Files\\chrome-win\\chrome.exe", // Update this path if needed
+    headless: true,
   });
+
+  const page = await browser.newPage();
+  await page.setContent(htmlContent, { waitUntil: "networkidle0" });
+
+  const pdfBuffer = await page.pdf({
+    format: "A4",
+    printBackground: true,
+  });
+
+  await browser.close();
+  return pdfBuffer;
 }
